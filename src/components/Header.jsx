a6ea94f49565234c89c2dbbcc29fd12c97bf2430
Daniël <danielmeijs@gmail.com>
2025-02-19 11:51:39 +0100

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="/public/globe.png" alt="Golbe icon" aria-hidden="true"/>
+                <span>My travel journal.</span>
+            </nav>
+        </header>
+    )
+}
