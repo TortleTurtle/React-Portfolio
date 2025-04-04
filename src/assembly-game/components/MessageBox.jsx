16fe0d7c05e5e1815e96047155c8b20780b2650e
TortleTurtle <danielmeijs@gmail.com>
2025-04-04 18:09:13 +0200

    Assembly Game: Implemented win and lose conditions. Show guessed letters of word.

--- src/assembly-game/components/MessageBox.jsx
+++ src/assembly-game/components/MessageBox.jsx
@@ -1,8 +1,32 @@
-export default function MessageBox() {
+export function MessageBox(props) {
+
+    //Finally found a (completely unnecessary) use case for var.
+    switch (props.type) {
+        case messageTypes.SUCCESS:
+            var modifier = "messageBox-success"; // this works because the var declaration gets 'hoisted'
+            break;
+        case messageTypes.ERROR:
+            modifier = "messageBox-error";
+            break;
+        case messageTypes.INFO:
+            modifier = "messageBox-info";
+            break;
+        default:
+            modifier = "";
+            break;
+    }
+
     return (
-        <section className="messageBox messageBox-success">
-            <h2>You win!</h2>
-            <p>Well done!🎉</p>
+        <section className={"messageBox " + modifier}>
+            {props.title && <h2>{props.title}</h2>}
+            {props.subtitle && <p>{props.subtitle}</p>}
         </section>
     )
 }
+
+export const messageTypes = Object.freeze({
+    NONE: 0,
+    SUCCESS: 1,
+    ERROR: 2,
+    INFO: 3,
+})
